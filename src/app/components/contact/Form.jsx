e8e0a5002daffa91ca446808836fa4d287cad4a2
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-11-05 15:16:03 +0200

    add  sonner toast feedback

--- src/app/components/contact/Form.jsx
+++ src/app/components/contact/Form.jsx
@@ -2,11 +2,21 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
+import { Toaster, toast } from 'sonner'
+import { MailCheck, MailX } from 'lucide-react';
 
 export default function Form() {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const sendEmail = (params) => {
+
+        const toastId = toast.loading('Sending your message...',
+            {
+                style: {
+                    backgroundColor: '#1B1B1B',
+                    border: none,
+                },
+            });
 
         emailjs
             .send(process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, params, {
@@ -18,9 +28,27 @@ export default function Form() {
             .then(
                 () => {
                     console.log('SUCCESS!');
+                    toast.success('Message sent successfully, I will get back to you soon!', {
+                        id: toastId,
+                        duration: 5000,
+                        icon: <MailCheck />,
+                        style: {
+                            backgroundColor: '#1B1B1B',
+                            border: 'none',
+                        },
+                    });
                 },
                 (error) => {
                     console.log('FAILED...', error.text);
+                    toast.error('Message failed to send!', {
+                        id: toastId,
+                        duration: 5000,
+                        icon: <MailX />,
+                        style: {
+                            backgroundColor: '#1B1B1B',
+                            border: 'none',
+                        },
+                    });
                 },
             );
     };
@@ -39,49 +67,52 @@ export default function Form() {
     console.log(errors);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}
-            className='max-w-md w-full items-center flex flex-col justify-center space-y-4'>
-            {/* name input */}
-            <input type="text" placeholder="name" {...register("name", {
-                required: 'This field is required',
-                minLength: {
-                    value: 2,
-                    message: 'Name is too short. Min: 2 characters'
-                },
-                maxLength: {
-                    value: 30,
-                    message: 'Name is too long. Max: 30 characters'
+        <>
+            <Toaster position="bottom-left" richColors />
+            <form onSubmit={handleSubmit(onSubmit)}
+                className='max-w-md w-full items-center flex flex-col justify-center space-y-4'>
+                {/* name input */}
+                <input type="text" placeholder="name" {...register("name", {
+                    required: 'This field is required',
+                    minLength: {
+                        value: 2,
+                        message: 'Name is too short. Min: 2 characters'
+                    },
+                    maxLength: {
+                        value: 30,
+                        message: 'Name is too long. Max: 30 characters'
+                    }
+                })}
+                    className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg' />
+                {
+                    errors.name && <span className='inline-block self-start text-red-500'>{errors.name.message}</span>
                 }
-            })}
-                className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg' />
-            {
-                errors.name && <span className='inline-block self-start text-red-500'>{errors.name.message}</span>
-            }
-            
-            {/* email input */}
-            <input type="text" placeholder="email" {...register("email", { required: 'This field is required', pattern: /^\S+@\S+$/i })}
-                className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg' />
-            {
-                errors.email && <span className='inline-block self-start text-red-500'>{errors.email.message}</span>
-            }
 
-            {/* message input */}
-            <textarea placeholder='message' {...register("message", {
-                required: 'This field is required', maxLength: {
-                    value: 500,
-                    message: 'Message is too long. Max: 500 characters'
-                }, minLength: {
-                    value: 20,
-                    message: 'Message is too short. Min: 20 characters'
+                {/* email input */}
+                <input type="text" placeholder="email" {...register("email", { required: 'This field is required', pattern: /^\S+@\S+$/i })}
+                    className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg' />
+                {
+                    errors.email && <span className='inline-block self-start text-red-500'>{errors.email.message}</span>
                 }
-            })}
-                className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg' />
-            {
-                errors.message && <span className='inline-block self-start text-red-500'>{errors.message.message}</span>
-            }
-            <input
-                value={'Cast your message!'}
-                className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize' type="submit" />
-        </form>
+
+                {/* message input */}
+                <textarea placeholder='message' {...register("message", {
+                    required: 'This field is required', maxLength: {
+                        value: 500,
+                        message: 'Message is too long. Max: 500 characters'
+                    }, minLength: {
+                        value: 20,
+                        message: 'Message is too short. Min: 20 characters'
+                    }
+                })}
+                    className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg' />
+                {
+                    errors.message && <span className='inline-block self-start text-red-500'>{errors.message.message}</span>
+                }
+                <input
+                    value={'Cast your message!'}
+                    className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize' type="submit" />
+            </form>
+        </>
     );
 }
