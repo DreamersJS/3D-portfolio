d83af9dac3b728eb2bf76a623b86cb6f0d67aaac
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-12-13 19:16:19 +0200

    add animations using framer-motion

--- src/app/components/nav/NavCircle.jsx
+++ src/app/components/nav/NavCircle.jsx
@@ -4,12 +4,23 @@ import React from 'react';
 import NavButton from './NavButton';
 import useScreenSize from '../hooks/useScreenSize';
 import ResponsiveComponent from '../ResponsiveComponent';
+import { motion } from 'framer-motion';
+
+const container = {
+    hidden: { opacity: 0 },
+    show: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.3
+        }
+    }
+}
 
 const Nav = () => {
 
     const angleIncrement = 360 / BtnList.length;
     const { width, height } = useScreenSize();
-    console.log("Nav Component - width:", width, height);
+
     if (width === null || height === null) {
         return <div>Loading...</div>;
     }
@@ -24,7 +35,12 @@ const Nav = () => {
                         // Desktop Layout
                         if (width && width >= 480 && height > 480) {
                             return (
-                                <div className='flex items-center justify-center relative hover:pause group animate-spin-slow'>
+                                <motion.div
+                                    variants={container}
+                                    initial='hidden'
+                                    animate='show'
+
+                                    className='flex items-center justify-center relative hover:pause group animate-spin-slow'>
                                     {
                                         BtnList.map((btn, index) => {
                                             const angleRadiant = (angleIncrement * index * Math.PI) / 180;
@@ -35,24 +51,16 @@ const Nav = () => {
                                             return <NavButton key={btn.label} x={x} y={y} {...btn} />
                                         })
                                     }
-                                </div>)
-                        } else if ( width >= 480 && width > height) {
+                                </motion.div>)
+                        } else if (width >= 480 && width > height) {
                             // Mobile Landscape Layout
                             return (
                                 <>
-                                    {/* Top side buttons of the Mobile Nav */}
-                                    {/* <div className='w-full px-2.5 flex flex-row space-x-4 items-center justify-center py-4'>
-                                        {
-                                            BtnList.slice(0, BtnList.length / 2).map((btn) => {
-                                                return <div key={btn.label} className='p-2 m-4'>
-                                                    <NavButton key={btn.label} x={0} y={0} {...btn} />
-                                                </div>
-                                            })
-                                        }
-                                    </div> */}
-                            
-                                    {/* Bottom side buttons of the Mobile Nav */}
-                                    <div className='w-full flex flex-row flex-wrap justify-evenly items-start mx-2'>
+                                    <motion.div
+                                        variants={container}
+                                        initial='hidden'
+                                        animate='show'
+                                        className='w-full flex flex-row flex-wrap justify-evenly items-start mx-2'>
                                         {
                                             BtnList.map((btn) => {
                                                 return <div key={btn.label} className=''>
@@ -60,33 +68,40 @@ const Nav = () => {
                                                 </div>
                                             })
                                         }
-                                    </div>
+                                    </motion.div>
                                 </>
                             )
-                            
+
                         } else {
                             // Mobile (Portrait) Layout
                             return (
                                 <>
                                     {/* Left side buttons of the Mobile Nav */}
-                                    <div className='w-full px-2.5 xs:p-0 xs:w-max flex flex-col space-y-32 items-start xs:items-center justify-center relative group'>
+                                    <motion.div
+                                        variants={container}
+                                        initial='hidden'
+                                        animate='show'
+                                        className='w-full px-2.5 xs:p-0 xs:w-max flex flex-col space-y-32 items-start xs:items-center justify-center relative group'>
                                         {
                                             BtnList.slice(0, BtnList.length / 2).map((btn) => {
                                                 return <div key={btn.label} className='p-2'> <NavButton key={btn.label} x={0} y={0} {...btn} /></div>
                                             })
                                         }
-                                    </div>
+                                    </motion.div>
                                     {/* Right side buttons of the Mobile Nav */}
-                                    <div className='w-full px-2.5 xs:p-0 xs:w-max flex flex-col space-y-32 items-end xs:items-center justify-center relative group'>
+                                    <motion.div
+                                        variants={container}
+                                        initial='hidden'
+                                        animate='show' className='w-full px-2.5 xs:p-0 xs:w-max flex flex-col space-y-32 items-end xs:items-center justify-center relative group'>
                                         {
                                             BtnList.slice(BtnList.length / 2, BtnList.length).map((btn) => {
                                                 return <div key={btn.label} className='mr-12 px-1'><NavButton key={btn.label} x={0} y={0} {...btn} labelDirection="left" /></div>
                                             })
                                         }
-                                    </div>
+                                    </motion.div>
                                 </>
                             )
-                            
+
                         }
 
                     }
