15fe81c52bfcd1949d6d37b5102183fe45e1378c
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-12-13 03:24:14 +0200

    add responsiveness to projects

--- src/app/components/projects/index.jsx
+++ src/app/components/projects/index.jsx
@@ -4,7 +4,7 @@ const ProjectList = ({ projects }) => {
     return (
         <div className="flex flex-col items-center justify-between">
             <h1 className="text-4xl font-bold mb-4 -mt-16">Projects</h1>
-            <div className="w-full max-w-4kl px-16 space-y-8 flex flex-col items-center">
+            <div className="w-full max-w-auto xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center">
                 {projects.map((project) => (
                     <ProjectLayout key={project.id} {...project} />
                 ))}
