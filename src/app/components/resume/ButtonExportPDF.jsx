1035f19b94770cbccdec4a5d5e076e54776105e8
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-12-14 14:16:00 +0200

    add download pdf
    finally working
    with html2pdf,
    added page breaks also

--- src/app/components/resume/ButtonExportPDF.jsx
+++ src/app/components/resume/ButtonExportPDF.jsx
@@ -1,10 +1,15 @@
+'use client';
 
-
-const ButtonExportPDF = () => {
+const ButtonExportPDF = (handlePrint) => {
     return (
-        <div>
-           
-        </div>
+        <div className="text-center mt-6">
+                <button
+                    onClick={handlePrint}
+                    className="px-4 py-2 bg-background text-white rounded-lg shadow-md"
+                >
+                    Download as PDF
+                </button>
+            </div>
     );
 };
 export default ButtonExportPDF;
